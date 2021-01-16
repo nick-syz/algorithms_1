1a92706e3ddb8b9d79a4f25936750351911970de
Nick <nicksyz13@gmail.com>
2021-01-16 17:22:13 +0300

    initial commit

---
+++ Brackets_tests.py
@@ -0,0 +1,14 @@
+from Brackets import Balanced
+from unittest import TestCase
+
+class Brackets_test(TestCase):
+    def test(self):
+        self.assertEqual(False, Balanced(''))
+        self.assertEqual(False, Balanced('('))
+        self.assertEqual(False, Balanced(')'))
+        self.assertEqual(True, Balanced('()'))
+        self.assertEqual(False, Balanced('())'))
+        self.assertEqual(True, Balanced('((()))'))
+        self.assertEqual(False, Balanced('(()))))'))
+        self.assertEqual(False, Balanced(')))))))'))
+        self.assertEqual(False, Balanced('(((((('))
