2cc64e61783c1fbe5c429b3fb2f1703df3845ede
Nick <nicksyz13@gmail.com>
2021-01-27 18:47:02 +0300

    initial commit

---
+++ BloomFilter.py
@@ -0,0 +1,28 @@
+# https://skillsmart.ru/algo/15-121-cm/z0b3da83s2.html
+
+class BloomFilter:
+
+    def __init__(self, f_len):
+        self.filter_len = f_len
+        self.arr = [0] * f_len
+
+    def hash1(self, str1):
+        i = 0
+        for c in str1:
+            i = (i*17 + ord(c))%10
+        return i
+
+    def hash2(self, str1):
+        j = 0
+        for c in str1:
+            j = (j*223 + ord(c))%10
+        return j
+
+    def add(self, str1):
+        self.arr[self.hash1(str1)] = 1
+        self.arr[self.hash2(str1)] = 1
+
+    def is_value(self, str1):
+        if self.arr[self.hash1(str1)] and self.arr[self.hash2(str1)]:
+            return True
+        return False
---
+++ BloomFilter_tests.py
@@ -0,0 +1,26 @@
+from BloomFilter import BloomFilter
+from unittest import TestCase
+
+class BloomFilterTest(TestCase):
+    def setUp(self):
+        self.bloom = BloomFilter(10)
+
+    def test_add(self):
+        self.bloom.add('0123456789')
+        self.assertEqual(True, self.bloom.is_value('0123456789'))
+
+        self.bloom.add('1234567890')
+        self.bloom.add('2345678901')
+        self.bloom.add('3456789012')
+        self.bloom.add('4567890123')
+        self.bloom.add('5678901234')
+        self.bloom.add('6789012345')
+        self.bloom.add('7890123456')
+        
+        # self.assertEqual(False, self.bloom.is_value('8901234567'))
+        # self.assertEqual(False, self.bloom.is_value('9012345678'))
+        # self.assertEqual(False, self.bloom.is_value('0123456789'))
+
+        self.bloom.add('8901234567')
+        self.bloom.add('9012345678')
+        self.bloom.add('0123456789')
