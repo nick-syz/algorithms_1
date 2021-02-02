0f816cc571fac88975b418f7e8a27ce10987174e
Nick <nicksyz13@gmail.com>
2021-02-02 10:45:27 +0300

    third commit

--- BloomFilter.py
+++ BloomFilter.py
@@ -4,25 +4,24 @@ class BloomFilter:
 
     def __init__(self, f_len):
         self.filter_len = f_len
-        self.arr = bytearray(f_len)
+        self.mask = 0
 
     def hash1(self, str1):
         i = 0
         for c in str1:
             i = (i*17 + ord(c)) % self.filter_len
-        return i
+        return 1 << i
 
     def hash2(self, str1):
         j = 0
         for c in str1:
             j = (j*223 + ord(c)) % self.filter_len
-        return j
+        return 1 << j
 
     def add(self, str1):
-        self.arr[self.hash1(str1)] = 1
-        self.arr[self.hash2(str1)] = 1
+        self.mask = self.mask | self.hash1(str1) | self.hash2(str1)
 
     def is_value(self, str1):
-        if self.arr[self.hash1(str1)] and self.arr[self.hash2(str1)]:
+        if self.mask == self.mask & (self.hash1(str1) | self.hash2(str1)):
             return True
         return False
