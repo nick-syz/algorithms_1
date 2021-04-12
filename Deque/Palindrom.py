58e0d9c4c6d65f3c35d51a4aa5ffc0585e35c91a
Nick <nicksyz13@gmail.com>
2021-04-12 15:45:06 +0300

    Palindrom cleaning: Done

--- Deque/Palindrom.py
+++ Deque/Palindrom.py
@@ -4,12 +4,12 @@ def palindrom(string):
     if not len(string):
         raise 'String is an empty!'
     deque = Deque()
-    count = 0
+    same_elements_count = 0
     for i in range(len(string)//2):
         deque.addFront(string[i])
         deque.addTail(string[-(i+1)])
         if deque.removeFront() == deque.removeTail():
-            count += 1
-    if count == len(string)//2:
+            same_elements_count += 1
+    if same_elements_count == len(string)//2:
         return True
     return False
